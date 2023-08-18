4b860b007154db6d27b3ee571153cff658ab23d0
조봉균 <kjbg4565388@naver.com>
2023-08-18 15:58:29 +0900

    뷰 초기작업 및 데이터 저장 로직작업

---
+++ src/main/java/com/example/SummerProject/entity/Board.java
@@ -0,0 +1,23 @@
+package com.example.SummerProject.entity;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@Getter
+public class Board {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column
+    private String title;
+    @Column
+    private String content;
+}
